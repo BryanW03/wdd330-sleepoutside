484ae1e6ab7396c5a745dc95d12d582c8a7f9780
Brayan De Leon <bryanw628@gmail.com>
2026-05-16 17:11:46 -0400

    wee2k

--- src/js/product.js
+++ src/js/product.js
@@ -6,15 +6,15 @@ const dataSource = new ProductData('tents');
 function addProductToCart(product) {
   // 1. Intentamos obtener el contenido actual del carrito en LocalStorage
   let cartItems = getLocalStorage('so-cart');
-  
+
   // 2. Si no es una lista válida (está vacío o es la primera vez), inicializamos un arreglo vacío []
   if (!Array.isArray(cartItems)) {
     cartItems = [];
   }
-  
+
   // 3. Agregamos el nuevo producto seleccionado a la lista existente
   cartItems.push(product);
-  
+
   // 4. Guardamos la lista completa actualizada de vuelta en el LocalStorage
   setLocalStorage('so-cart', cartItems);
 }
