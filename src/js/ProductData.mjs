b12431f2f7c099a982ed9f349ad9c019df01b81a
Brayan De Leon <bryanw628@gmail.com>
2026-06-07 17:59:47 -0400

    week5

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -6,6 +6,32 @@ function convertToJson(res) {
   }
 }
 
+// Fix image paths: replace ../images/ with /images/
+function fixImagePaths(data) {
+  return data.map(item => {
+    // Fix main image
+    if (item.Image) {
+      item.Image = item.Image.replace(/^\.\.\/images\//, '/images/');
+    }
+    // Fix Images object
+    if (item.Images) {
+      Object.keys(item.Images).forEach(key => {
+        if (typeof item.Images[key] === 'string') {
+          item.Images[key] = item.Images[key].replace(/^\.\.\/images\//, '/images/');
+        }
+      });
+    }
+    // Fix Colors ColorImg
+    if (item.Colors && Array.isArray(item.Colors)) {
+      item.Colors = item.Colors.map(c => ({
+        ...c,
+        ColorImg: c.ColorImg ? c.ColorImg.replace(/^\.\.\/images\//, '/images/') : c.ColorImg
+      }));
+    }
+    return item;
+  });
+}
+
 export default class ProductData {
   constructor(category) {
     this.category = category;
@@ -15,7 +41,10 @@ export default class ProductData {
   async getData() {
     return fetch(this.path)
       .then(convertToJson)
-      .then((data) => data.Result || data);
+      .then((data) => {
+        const list = data.Result || data;
+        return fixImagePaths(list);
+      });
   }
 
   async findProductById(id) {
